86fbdcdc23d498929121534179488c6fc7746501
angelo-francisco <ics20080729@gmail.com>
2024-07-12 12:43:32 +0100

    STATIC URL AND ROOT added again

--- core/settings.py
+++ core/settings.py
@@ -107,6 +107,10 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    (os.path.join(BASE_DIR / 'extras/static'))
+]
 STATIC_ROOT = os.path.join(BASE_DIR / "staticfiles_build")
 
 MEDIA_URL = "/media/"
