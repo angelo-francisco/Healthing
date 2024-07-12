981d7c6ea1bde649dedbd2fb65c5a57bc0b60b3c
angelo-francisco <ics20080729@gmail.com>
2024-07-12 12:25:14 +0100

    Deploy initial impls

--- core/settings.py
+++ core/settings.py
@@ -65,12 +65,12 @@ WSGI_APPLICATION = "core.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "extras/db/db.sqlite3",
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "extras/db/db.sqlite3",
+#     }
+# }
 
 
 # Password validation
@@ -107,9 +107,7 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "/static/"
-STATICFILES_DIRS = (os.path.join(BASE_DIR / "extras/static"),)
-STATIC_ROOT = os.path.join(BASE_DIR / "static")
+STATIC_ROOT = os.path.join(BASE_DIR / "staticfiles_build", "static")
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR / "extras/media")
