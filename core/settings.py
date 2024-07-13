a0801df4e5a0c2017632a192241ae7672eeba958
angelo-francisco <ics20080729@gmail.com>
2024-07-13 21:05:15 +0100

    Fix deployment issue v1.2

--- core/settings.py
+++ core/settings.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -42,6 +43,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = "core.urls"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 TEMPLATES = [
     {
--- core/wsgi.py
+++ core/wsgi.py
@@ -1,9 +1,8 @@
 import os
-
+from whitenoise import WhiteNoise
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
 application = get_wsgi_application()
-
 app = application
